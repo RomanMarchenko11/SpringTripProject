56162427b3a2361f48066e9cf1a79332957909dd
Roman Marchenko <rmarchenko11@icloud.com>
2023-09-29 12:05:47 +0300

    first commit

--- src/main/java/com/r/dreams_2/configuration/SecurityConfig.java
+++ src/main/java/com/r/dreams_2/configuration/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.r.dreams_2.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +16,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.net.PasswordAuthentication;
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
